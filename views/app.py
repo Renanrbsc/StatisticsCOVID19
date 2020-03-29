4d1749a704634e62cb729f67760a3eeade57a0b4
Renan Berti da Cruz Ribas <renan.ribas@hbsis.com.br>
2020-03-29 19:20:14 -0300

    search the data country in date 28-03-20 daily reports

--- views/app.py
+++ views/app.py
@@ -1,50 +1,41 @@
 # importing csv module 
+import sys
+sys.path.append(r"C:\Users\renan.ribas\Documents\Github")
+
 import csv 
-  
-# csv file name 
-filename = r"C:\Users\renan.ribas\Documents\Github\COVID-19\csse_covid_19_data\csse_covid_19_daily_reports\03-28-2020.csv"
-  
-# initializing the titles and rows list 
-fields = [] 
-rows = [] 
-  
-# reading csv file 
-with open(filename, 'r') as csvfile: 
-    # creating a csv reader object 
-    csvreader = csv.reader(csvfile) 
+from StatisticsCOVID19.views.file_management import ManagerFile
 
-    # extracting field names through first row 
-    fields = next(csvreader) 
-    next(csvreader) 
-  
-    # extracting each data row one by one 
-    for row in csvreader: 
-        rows.append(row) 
-  
-    # get total number of rows 
-    print(f"Total number of rows: {csvreader.line_num}") 
-  
-# printing the field names 
-print('Field names are: ' + ', '.join(field for field in fields)) 
-  
-#  printing first 5 rows 
-print('\nFirst 5 rows are:\n') 
-for row in rows[:5]: 
-    # parsing each column of a row 
-    for index, column in enumerate(row): 
-        print(f"{fields[index]}: {column}"), 
-    print('\n') 
+class RequestsDataFile:
+    def __init__(self):
+        self._manager_file = ManagerFile()
 
-def search_name_country():
-    name_country = input('Search country: ')
-    print(f'\nSearch name of country: {name_country}\n') 
-    for row in rows: 
-        if name_country == row[3]:
-        # parsing each column of a row 
-            return row
+    def loading_manager_methods(self):
+        self._manager_file.choice_a_filename()
+        self._manager_file.load_file_csv()
 
-row = search_name_country()
-for index, column in enumerate(row): 
-    print(f"{fields[index]}: {column}"), 
-print('\n') 
+    def search_the_first_five_countries(self):      
+        # printing first 5 rows 
+        print('\nFirst 5 rows are:\n') 
+        for row in self._manager_file.rows[:5]: 
+            # parsing each column of a row 
+            for index, column in enumerate(row): 
+                print(f"{self._manager_file.fields[index]}: {column}"), 
+            print('\n') 
+
+    def search_name_country(self) -> None:
+        name_country = input('Search country: ')
+        print(f'\nSearch name of country: {name_country}\n') 
+        for row in self._manager_file.rows: 
+            if name_country == row[3]:
+            # parsing each column of a row 
+                for index, column in enumerate(row): 
+                    print(f"{self._manager_file.fields[index]}: {column}") 
+                return 
+
+
+
+datafile = RequestsDataFile()
+datafile.loading_manager_methods()
+datafile.search_name_country()
+
 
