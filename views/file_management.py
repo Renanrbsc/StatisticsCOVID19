9278c18ba1a96b34dee1ea12369f6fe282531a26
Renan Berti Ribas <renanrbsc@gmail.com>
2020-03-29 22:48:42 -0300

    request csv in url

--- views/file_management.py
+++ views/file_management.py
@@ -1,25 +1,38 @@
 import csv
-
+import requests
+from contextlib import closing
 
 class ManagerFile:
+    BASE_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/'
+    CONFIRMED = 'time_series_covid19_confirmed_global.csv'
+    DEATH = 'time_series_covid19_deaths_global.csv'
+    RECOVERED = 'time_series_covid19_recovered_global.csv'
+    
     def __init__(self):
         self.fields = [] 
         self.rows = [] 
 
     def choice_a_filename(self) -> None:
-        date = input('Insert a date from daily reports: ')
-        self.filename_daily_report = r"C:\Users\renan.ribas\Documents\Github\COVID-19\csse_covid_19_data\csse_covid_19_daily_reports\03-28-2020.csv"
+        # date = input('Insert a date from daily reports: ')
+        self.filename_daily_report = f"{self.BASE_URL}{self.CONFIRMED}"
 
     def load_file_csv(self) -> None:
-        with open(self.filename_daily_report, 'r') as csvfile: 
-            csvreader = csv.reader(csvfile) 
+        url = f"{self.filename_daily_report}"
 
-            self.fields = next(csvreader) 
-            next(csvreader) 
+        with closing(requests.get(url, stream=True)) as r:
+            csvfile = r.iter_lines()
 
-            for row in csvreader:
+            self.fields = next(csvfile) 
+            next(csvfile) 
+
+            for row in csvfile:
                 self.rows.append(row)  
+                print(row)  
 
-            # get total number of rows 
-            print(f"Total number of rows: {csvreader.line_num}") 
-  
+        # get total number of rows 
+        print(f"Total number of rows: {csvfile.line_num}") 
+
+c = ManagerFile()
+c.choice_a_filename()
+c.load_file_csv()
+
