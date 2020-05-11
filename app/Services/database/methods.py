05bc1c6e9d0245c800c90c8e6c2ef917284df650
Renan Berti da Cruz Ribas <renan.ribas@hbsis.com.br>
2020-05-11 08:23:46 -0300

    load archives for methods

--- app/Services/database/methods.py
+++ app/Services/database/methods.py
@@ -1,5 +1,8 @@
+from app.Model.file_management import ManagerFile
+
 class Database:
     def __init__(self):
+        self._manager_file = ManagerFile()
         self.name_archive = 'confirmed'
         self.fields = []
         self.rows = []
@@ -17,12 +20,23 @@ class Database:
             print(f"{name_archive.capitalize()} data updated successfully!")
 
 
-    def load_data(self):
-        with open(f'app\Services\{self.name_archive}.txt', 'r') as arc:
+    def load_data(self, option):
+        name_archive = str()
+
+        for filename in self._manager_file.filenames:
+            if option == 1 and filename == 'confirmed':
+                name_archive = filename
+            elif option == 2 and filename == 'death':
+                name_archive = filename 
+            elif option == 3 and filename == 'recovered':
+                name_archive = filename 
+            
+        with open(f'app\Services\{name_archive}.txt', 'r') as arc:
             for row in arc:
                 row = row.strip()
                 new_row = row.split(',')
                 self.rows.append(new_row)
             self.fields = self.rows.pop(0)
         print("Data loaded successfully!")
+        
         return self.fields, self.rows
