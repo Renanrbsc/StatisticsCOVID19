c41d3b74eb9a9100ee6f3b8e9b8ff650aaedd1bd
Renan Berti da Cruz Ribas <renan.ribas@hbsis.com.br>
2020-05-11 08:24:14 -0300

    choose archive for methods

--- app/Process/process_quest.py
+++ app/Process/process_quest.py
@@ -5,16 +5,25 @@ class StatisticsSearch:
     def __init__(self):
         self._methods = Methods()
         self._db = Database()
+        self.fields = list()
+        self.rows = list()
+    
+    def choose_type_archive(self):
+        print("-------- Search data in -----------\n" \
+              " 1 - Confirmed\n" \
+              " 2 - Death\n" \
+              " 3 - Recovered\n")
+        option = int(input('Enter the desired data:'))
+
+        self.fields, self.rows = self._db.load_data(option)
 
     def method_menu(self):
-        fields, rows = self._db.load_data()
-
         print("-------- Search methods -----------\n" \
               " 1 - Search by country name\n" \
               " 2 - Search for the top 5\n")
         option = int(input('Enter the desired method:'))
 
         if option == 1:
-            self._methods.search_name_country(fields, rows)
+            self._methods.search_name_country(self.fields, self.rows)
         elif option == 2:
-            self._methods.top_five_biggest_cases(fields, rows)
+            self._methods.top_five_biggest_cases(self.fields, self.rows)
