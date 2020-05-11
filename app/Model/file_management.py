f31586106c09196f5899cc3566cfeab71491375b
Renan Berti da Cruz Ribas <renan.ribas@hbsis.com.br>
2020-05-11 07:22:13 -0300

    added acsess for data of the recovered and death

--- app/Model/file_management.py
+++ app/Model/file_management.py
@@ -11,37 +11,43 @@ class ManagerFile:
     RECOVERED = 'time_series_covid19_recovered_global.csv'
     
     def __init__(self):
+        self.filenames = dict()
+        self.data_csv = list()
         self.rows = [] 
         self.fields = [] 
         self._dpross = DataProcess()
 
-    def choice_a_filename(self) -> None:
-        # date = input('Insert a date from daily reports: ')
-        self.filename = f"{self.BASE_URL}{self.CONFIRMED}"
+    def build_update_access(self) -> None:
+        self.filenames = {'confirmed':f"{self.BASE_URL}{self.CONFIRMED}",
+                          'death':f"{self.BASE_URL}{self.DEATH}",
+                          'recovered':f"{self.BASE_URL}{self.RECOVERED}"
+                          }
 
     def load_file_csv(self) -> None:
-        url = f"{self.filename}"
-       
-        try:
-            with closing(requests.get(url, stream=True)) as r:
-                csvfile = r.iter_lines()
-                
-                for row in csvfile:
-                    data_row = self._dpross.replace_characters(row, "b'", '')
-                    data_row = self._dpross.replace_characters(data_row, "'", '')
-                    data_row = self._dpross.replace_characters(data_row, '"', '')
-                    list_data_row = self._dpross.convert_string_to_list(data_row)
-                    self.rows.append(list_data_row)  
-                self.fields = self.rows.pop(0)
-                
-                return self.fields, self.rows
+        for filename in self.filenames:    
+            try:
+                with closing(requests.get(self.filenames[filename], stream=True)) as r:
+                    csvfile = r.iter_lines()
+                    
+                    for row in csvfile:
+                        data_row = self._dpross.replace_characters(row, "b'", '')
+                        data_row = self._dpross.replace_characters(data_row, "'", '')
+                        data_row = self._dpross.replace_characters(data_row, '"', '')
+                        list_data_row = self._dpross.convert_string_to_list(data_row)
+                        self.rows.append(list_data_row)  
+                    self.fields = self.rows.pop(0)
+                    
+                    self.data_csv.append([self.fields, self.rows, filename])
 
-        except MissingSchema:
-            print("HTTP link appears to be broken!")
-            return
-            
-        finally:
-            # get total number of rows 
-            print(f"Total number of rows: {len(self.rows)}") 
+
+            except MissingSchema:
+                print("HTTP link appears to be broken!")
+                return
+                
+            finally:
+                # get total number of rows 
+                print(f"Number of rows updated in the '{filename}' file: {len(self.rows)}") 
+
+        return self.data_csv
 
 
